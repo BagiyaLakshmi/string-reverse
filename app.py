657b2508e8701a1975035dc66f5c9cea11002440
Bagiyalakshmi <bagiyalakshmi59@gmail.com>
2023-07-22 21:42:17 +0530

    json add

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
-
-
-# result = reverse_string("Prabhu")
-
-# print(result)
-
 '''
 Source:
 
-Author: Raja CSP
-
+Author: Bagiya
 
 '''
 
@@ -17,10 +10,8 @@ from flask import Flask,render_template, request
 import random
 import json
 
-
 # Local import
-
-from models import reverse_string 
+from models import reverse_string, addition, update_json
 
 app  = Flask(__name__)
 PORT = 3011
@@ -35,6 +26,35 @@ def sample():
         input_string = request.form['my_name']
         reversed_string = reverse_string(input_string)
         return render_template('results.html', org = input_string, reversed_string=reversed_string)
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+
+@app.route('/add-result', methods= ['post'])
+def add_result():
+    if request.method == 'POST':
+        input1 = request.form['input1']
+        input2 = request.form['input2']
+        result = addition(int(input1), int(input2))
+        return render_template('add.html', result = result)
+        
+@app.route('/new-user')
+def user_update():
+    return render_template('user.html')
+
+@app.route('/user-detail', methods= ['post'])
+def user_detail():
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['age']
+        city = request.form['city']
+        update_json(name, int(age), city)
+        return render_template('user.html', msg = 'updated successfully!!!')
+
+
+
 
 if __name__ == "__main__":
     app.run( debug = True,host="0.0.0.0",port = PORT)
--- models.py
+++ models.py
@@ -6,21 +6,35 @@ Created on
 source:
 '''
 
-# 1. simple python 
-
-
-# print(str1[::-1])
-
+import json
 
 def reverse_string(str1):
 
     return(str1[::-1])
 
+def addition(a,b):
+    sum= a + b
+    return sum
+
+def update_json(name, age, city):
+    new_data ={
+        name :{
+            "age":age,
+            "city":city
+        }
+    }
+    with open('data.json') as file:
+        data_dict = json.load(file)
+        print(data_dict)
+    data_dict.update(new_data)
+    print(data_dict)
+    with open ("data.json", "w")as f:
+        json.dump(data_dict, f,  indent = 4)
+
 
 if __name__ == '__main__':
 
-    resut = reverse_string("Ganesh")
-
-    # print(resut)
+    # resut = reverse_string("Ganesh")
+    update_json('Deepika', 17, 'Los Vegas')
 
 
